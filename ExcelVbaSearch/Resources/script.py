ed23f3b4d38c8554732940f334b95d46c7d62dcf
Adrian Sikorski <adi52@tlen.pl>
2025-02-11 22:15:59 +0100

    Dodanie ignorowanie wielkości znaków w skrypcie

--- ExcelVbaSearch/Resources/script.py
+++ ExcelVbaSearch/Resources/script.py
@@ -5,7 +5,7 @@ import sys
 
 def search_vba_in_excels(folder_path, search_string):
     matching_files = []  # Lista plików z podaną frazą
-
+    search_string_lower = search_string.lower()  # Konwersja szukanego tekstu na małe litery
 
     for root, _, files in os.walk(folder_path):
         for file in files:
@@ -14,10 +14,10 @@ def search_vba_in_excels(folder_path, search_string):
                 try:
                     # Uruchomienie olevba bez hasła
                     result = subprocess.run(["python", "-m", "oletools.olevba", file_path],
-               capture_output=True, text=True)
-                    # Sprawdzenie, czy fraza jest w kodzie VBA
-                    if search_string in result.stdout:
-                        matching_files.append(file_path)                       
+                                            capture_output=True, text=True)
+                    # Sprawdzenie, czy fraza jest w kodzie VBA (ignorując wielkość liter)
+                    if search_string_lower in result.stdout.lower():
+                        matching_files.append(file_path)
                 except Exception as e:
                     print(f"Błąd podczas przetwarzania pliku {file_path}: {e}")
 
